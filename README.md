2eeafa9bb17dba15ec239fd2f72bdcb6d33e2f91
MP281X <paludgnachmatteo.dev@gmail.com>
2024-05-04 16:56:20 +0200

    project setup

---
+++ README.md
@@ -0,0 +1,16 @@
+```jsonc
+{
+	"extends": ["./configs/tsconfig.json"],
+	"include": ["**/*.js", "**/*.ts"],
+	"exclude": ["**/node_modules", "**/.*/", "**/dist"]
+}
+```
+
+
+```js
+export { default } from './configs/eslint.js'
+```
+
+```js
+export { default } from './configs/eslint.js'
+```
---
+++ configs/prettier.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+	semi: false,
+	useTabs: true,
+	printWidth: 150,
+	singleQuote: true,
+	trailingComma: 'none',
+	arrowParens: 'always',
+	quoteProps: 'consistent',
+	experimentalTernaries: true,
+	overrides: [{ files: '*.json', options: { parser: 'jsonc' } }],
+	plugins: ['prettier-plugin-svelte', 'prettier-plugin-astro', 'prettier-plugin-tailwindcss']
+}
