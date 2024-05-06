c003aa17d2c51ba21ea178dd7aa1352052a3eec4
MP281X <paludgnachmatteo.dev@gmail.com>
2024-05-06 17:46:47 +0200

    improved the task-runner

--- src/lib/execCmd.ts
+++ src/lib/execCmd.ts
@@ -1,13 +1,24 @@
 import { log } from './logger.ts'
 import { spawn } from 'child_process'
+import fs from 'fs'
+
+export const getPackageManager = () => {
+	const currentDir = fs.readdirSync('.')
+
+	if (currentDir.includes('bun.lockb')) return 'bun'
+	if (currentDir.includes('pnpm-lock.yaml')) return 'pnpm'
+	if (currentDir.includes('package-lock.json')) return 'npm'
+	if (currentDir.includes('yarn.lock')) return 'yarn'
+
+	return 'pnpm'
+}
 
 export const asyncCommands: Promise<void>[] = []
 
 type ExecCmd = { title: string; cmd: string; mode: 'sync' | 'async'; cwd?: string }
 export const execCmd = async ({ title, cmd, cwd, mode }: ExecCmd) => {
 	const execPromise = new Promise<void>((resolve, _) => {
-		const [command, ...args] = cmd.split(' ') as [string, ...string[]]
-		const output = spawn(command, args, {
+		const output = spawn(getPackageManager(), cmd.split(' '), {
 			cwd: cwd ?? process.cwd(),
 			env: {
 				...process.env
--- src/task-runner.ts
+++ src/task-runner.ts
@@ -1,64 +1,68 @@
 #!/usr/bin/env node
-import { asyncCommands, execCmd } from './task-runner/execCmd.ts'
-import { findProjects } from './task-runner/findProjects.ts'
+import { asyncCommands, execCmd } from './lib/execCmd.ts'
 import readline from 'readline'
 
-// handle console clear and exit
-readline.emitKeypressEvents(process.stdin)
-process.stdin.setRawMode(true)
-process.stdin.on('keypress', (_, key: { ctrl: boolean; name: string }) => {
-	if (key.ctrl && key.name === 'c') return process.exit()
-	if (key.name === 'return') {
-		console.log('\n'.repeat(10000))
-		console.clear()
-	}
-})
-
-// find all the projects
-const projects = await findProjects()
-const task = (process.argv[2] ?? '') as ('dev' | 'build') & ({} & string) // eslint-disable-line @typescript-eslint/ban-types
-
-// lint/format
-if (task !== 'dev') {
-	// run prettier
-	const prettierArgs: string[] = ['--ignore-path .gitignore', '--log-level warn', '--cache', '--check']
-	await execCmd({
-		title: 'prettier',
-		cmd: `pnpm prettier ${prettierArgs.join(' ')} .`,
-		mode: 'sync'
-	})
-
-	// run eslint
-	const eslintArgs: string[] = ['--no-color', '--cache']
-	await execCmd({
-		title: 'eslint',
-		cmd: `pnpm eslint ${eslintArgs.join(' ')} .`,
-		mode: 'sync'
+if (process.stdin.isTTY) {
+	// handle console clear and exit
+	readline.emitKeypressEvents(process.stdin)
+	process.stdin.setRawMode(true)
+	process.stdin.on('keypress', (_, key: { ctrl: boolean; name: string }) => {
+		if (key.ctrl && key.name === 'c') return process.exit()
+		if (key.name === 'return') {
+			console.log('\n'.repeat(10000))
+			console.clear()
+		}
 	})
 }
 
-for (const { name, cwd } of projects) {
-	if (task === 'dev') continue
+const task = process.argv[2] as undefined | 'apply' | (string & {}) // eslint-disable-line @typescript-eslint/ban-types
+
+// apply formatting and linting rules
+if (task === 'apply') {
+	await execCmd({
+		title: 'prettier',
+		cmd: 'prettier --ignore-path .gitignore --log-level warn --cache --format .',
+		mode: 'sync'
+	})
+
+	await execCmd({
+		title: 'eslint',
+		cmd: 'eslint --no-color --cache --fix .',
+		mode: 'sync'
+	})
+
+	process.exit(0)
+}
+
+// check if all the formatting rules, linting rules and types are correct
+if (task === undefined) {
+	await execCmd({
+		title: 'prettier',
+		cmd: 'prettier --ignore-path .gitignore --log-level warn --cache --check .',
+		mode: 'sync'
+	})
+
+	await execCmd({
+		title: 'eslint',
+		cmd: 'eslint --no-color --cache .',
+		mode: 'sync'
+	})
 
 	// type check
 	await execCmd({
-		title: `${name}:tsc`,
-		cmd: 'pnpm tsc --noEmit',
-		mode: 'sync',
-		cwd
+		title: 'tsc',
+		cmd: 'tsc --noEmit',
+		mode: 'sync'
 	})
+
+	process.exit(0)
 }
 
-// run the scripts
-for (const { name, cwd, scripts } of projects) {
-	if (!scripts.includes(task)) continue
-
-	await execCmd({
-		title: `${name}:${task}`,
-		cmd: `pnpm run --silent ${task}`,
-		mode: 'async',
-		cwd
-	})
-}
+// execute a cmd
+await execCmd({
+	title: 'cmd',
+	cmd: task,
+	mode: 'async'
+})
 
 await Promise.all(asyncCommands).then(() => process.exit(0))
