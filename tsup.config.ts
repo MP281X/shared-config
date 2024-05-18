c73ba73f0ead8a4c59080bfa34274df5b4b5b762
MP281X <paludgnachmatteo.dev@gmail.com>
2024-05-18 13:18:51 +0200

    improvement

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
 	format: ['esm'],
 	outDir: 'dist',
 	shims: true,
+	silent: true,
 	splitting: false,
-	treeshake: true
+	treeshake: true,
+	// eslint-disable-next-line @typescript-eslint/require-await
+	onSuccess: async () => console.log('\x1b[36m%s\x1b[0m', '⚡ Build success')
 })
