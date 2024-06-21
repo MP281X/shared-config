da943e9a7187fca37ef53f2e587dbe44678e6ff4
MP281X <paludgnachmatteo.dev@gmail.com>
2024-06-21 11:42:00 +0200

    fix svelte?

--- configs/eslint.ts
+++ configs/eslint.ts
@@ -7,6 +7,7 @@ import fs from 'node:fs'
 // @ts-expect-error: no type definitions
 import eslint from '@eslint/js'
 import ts from 'typescript-eslint'
+import * as tsParser from '@typescript-eslint/parser'
 import type { ConfigWithExtends } from 'typescript-eslint'
 
 // @ts-expect-error: no type definitions
@@ -57,7 +58,7 @@ export default ts.config(
 	{ ignores: gitignore() },
 
 	{
-		languageOptions: { parser: ts.parser as any, parserOptions: { project: true } },
+		languageOptions: { parser: tsParser, parserOptions: { project: true } },
 		linterOptions: { reportUnusedDisableDirectives: true }
 	},
 
@@ -273,24 +274,31 @@ export default ts.config(
 	conditionalConfig('svelte', {
 		extends: svelte.configs['flat/recommended'] as any,
 		files: ['**/*.svelte'],
-		languageOptions: { parser: svelteParser, parserOptions: { extraFileExtensions: ['.svelte'], parser: ts.parser } },
-		rules: {
-			// eslint-disable-next-line unicorn/no-null
-			'svelte/block-lang': ['error', { enforceScriptPresent: true, script: ['ts'], style: ['postcss', null] }], // require lang="ts" in the script tag
-			'svelte/infinite-reactive-loop': 'error', // prevent reactivity bug
-			'svelte/no-export-load-in-svelte-module-in-kit-pages': 'error', // no function called load in script
-			'svelte/no-immutable-reactive-statements': 'error', // disable reactive statement for const values
-			'svelte/no-reactive-reassign': 'error', // don't readding derived reactive values
-			'svelte/no-store-async': 'error', // disable async await in stores
-			'svelte/no-useless-mustaches': 'error', // don't allow useless {}
-			'svelte/sort-attributes': 'off', // already checked by the "perfectionist" plugin
-			'svelte/valid-prop-names-in-kit-pages': 'error', // disable invalid exports in +page.svelte file
-
-			// rules that doesn't work in svelte 5
-			'@typescript-eslint/no-unsafe-assignment': 'off',
-			'@typescript-eslint/no-unsafe-call': 'off',
-			'@typescript-eslint/no-unsafe-member-access': 'off'
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				extraFileExtensions: ['.svelte'],
+				parser: tsParser,
+				project: true
+			}
 		}
+		// rules: {
+		// 	// eslint-disable-next-line unicorn/no-null
+		// 	'svelte/block-lang': ['error', { enforceScriptPresent: true, script: ['ts'], style: ['postcss', null] }], // require lang="ts" in the script tag
+		// 	'svelte/infinite-reactive-loop': 'error', // prevent reactivity bug
+		// 	'svelte/no-export-load-in-svelte-module-in-kit-pages': 'error', // no function called load in script
+		// 	'svelte/no-immutable-reactive-statements': 'error', // disable reactive statement for const values
+		// 	'svelte/no-reactive-reassign': 'error', // don't readding derived reactive values
+		// 	'svelte/no-store-async': 'error', // disable async await in stores
+		// 	'svelte/no-useless-mustaches': 'error', // don't allow useless {}
+		// 	'svelte/sort-attributes': 'off', // already checked by the "perfectionist" plugin
+		// 	'svelte/valid-prop-names-in-kit-pages': 'error', // disable invalid exports in +page.svelte file
+		//
+		// 	// rules that doesn't work in svelte 5
+		// 	'@typescript-eslint/no-unsafe-assignment': 'off',
+		// 	'@typescript-eslint/no-unsafe-call': 'off',
+		// 	'@typescript-eslint/no-unsafe-member-access': 'off'
+		// }
 	}),
 
 	// astro
