02bdbefc75f021fadaaf43769b31c5ee2668d012
MP281X <paludgnachmatteo.dev@gmail.com>
2024-06-22 18:41:07 +0200

    logger improvement

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -9,7 +9,6 @@ invalidLogs.push(
 	'VITE',
 	'vinxi ',
 	'hmr for .svelte',
-	'[vite] page reload',
 	'vite-plugin-svelte',
 	'Re-optimizing dependencies',
 	'➜  Network: use --host to expose',
@@ -44,8 +43,8 @@ invalidLogs.push(
 // astro
 invalidLogs.push('astro  v', '[types] Added', 'watching for file changes...', '┃ Network')
 
-// tsx
-invalidLogs.push('> ', "Completed running '")
+// tsx/pnpm
+invalidLogs.push('> ', "Completed running '", ' workspace projects')
 
 const strIncludes = (str: string, includes: string[]) => {
 	for (const include of includes) if (str.includes(include)) return true
@@ -53,16 +52,42 @@ const strIncludes = (str: string, includes: string[]) => {
 	return false
 }
 
-const formatLog = (input: string, type: LogType) => {
+const colors: Record<string, number> = {}
+const prefixColor = (prefix: string) => {
+	if (prefix in colors) return colors[prefix] ?? 0
+
+	const lastColor = Math.max(...Object.values(colors), 91)
+	colors[prefix] = lastColor >= 96 ? 94 : lastColor + 1
+
+	return colors[prefix] ?? 0
+}
+
+const formatLog = (input: string, type: LogType, prefix?: string) => {
 	for (const rawTxt of input.split('\n')) {
-		// biome-ignore lint/suspicious/noControlCharactersInRegex: <explanation>
+		if (rawTxt.trim().startsWith('{') && rawTxt.includes('"time"')) {
+			try {
+				const jsonLog = JSON.parse(rawTxt) as { wd: string; line?: string; stdio: 'stderr' | 'stdout' }
+				if (jsonLog.line) {
+					let projectName = jsonLog.wd.split('/').pop() ?? ''
+					if (projectName.length < 12) projectName += ' '.repeat(12 - projectName.length)
+
+					const logType: LogType = type === 'info' && jsonLog.stdio === 'stderr' ? 'error' : type
+					formatLog(jsonLog.line, logType, `\x1b[${prefixColor(projectName)}m${projectName} ➜ \x1b[${0}m`)
+				}
+			} catch {}
+
+			continue
+		}
+
+		// biome-ignore lint/suspicious/noControlCharactersInRegex:
 		let txt = rawTxt.replace(/\x1B\[[0-?]*[ -/]*[@-~]/g, '')
 
 		if (txt.trim() === '') continue
 		if (strIncludes(txt, invalidLogs)) continue
+		if (/^\w+ \w+\$ .+$/.test(txt)) continue
 
 		if (txt.includes('file truncated')) {
-			printLog('FILE CLEARED', 'warn')
+			printLog(`${prefix} FILE CLEARED`, 'warn', prefix)
 			continue
 		}
 
@@ -72,35 +97,33 @@ const formatLog = (input: string, type: LogType) => {
 			txt = txt.replace('- Local:', '')
 			txt = txt.replace('┃ Local', '')
 
-			printLog(txt.trim(), 'warn')
+			printLog(txt.trim(), 'warn', prefix)
 			continue
 		}
 
-		if (strIncludes(txt, ['✓ Compiled', 'hmr update'])) {
-			txt = txt.split(' ').filter(x => x.trim().startsWith('/'))[0] ?? ''
+		if (strIncludes(txt, ['✓ Compiled', 'hmr update', 'page reload'])) {
+			txt = txt.split(' ').filter(x => x.trim().includes('/'))[0] ?? ''
 			txt = txt.replaceAll(',', '')
 
-			printLog(`UPDATED ${txt.trim()}`, 'warn')
+			printLog(`UPDATED ${txt.trim()}`, 'warn', prefix)
 			continue
 		}
 
-		if (txt.startsWith("Restarting '")) {
-			printLog('RESTART', 'warn')
+		if (txt.includes("Restarting '")) {
+			printLog('RESTART', 'warn', prefix)
 			continue
 		}
 
-		printLog(rawTxt, type)
+		printLog(rawTxt, type, prefix)
 	}
 }
 
-export const printLog = (txt: string, type: LogType) => {
+export const printLog = (txt: string, type: LogType, prefix = '\n') => {
+	// biome-ignore format:
 	switch (type) {
-		case 'info':
-			return console.log(txt)
-		case 'warn':
-			return console.log('\n' + `\x1b[${93}m${txt}\x1b[${0}m`)
-		case 'error':
-			return console.log('\n' + `\x1b[${91}m${txt}\x1b[${0}m`)
+		case 'info':  return console.log(prefix + txt)
+		case 'warn':  return console.log(prefix + `\x1b[${93}m${txt}\x1b[${0}m`)
+		case 'error': return console.log(prefix + `\x1b[${91}m${txt}\x1b[${0}m`)
 	}
 }
 
@@ -115,7 +138,7 @@ const logFn = (type: LogType) => async (input: Input) => {
 
 type LogType = 'info' | 'warn' | 'error'
 export const log = {
-	error: logFn('error'),
 	info: logFn('info'),
-	warn: logFn('warn')
+	warn: logFn('warn'),
+	error: logFn('error')
 } satisfies Record<LogType, ReturnType<typeof logFn>>
