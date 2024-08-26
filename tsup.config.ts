42fbfc3a0916617aa7e3370503cca26c71bf9134
MP281X <paludgnachmatteo.dev@gmail.com>
2024-08-26 11:26:19 +0200

    renamed the utils package to stdlib

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,3 +1,3 @@
 import { tsupConfig } from './configs/tsup.ts'
 
-export default tsupConfig(['cli/index.ts', 'configs/tsup.ts', 'tests/index.ts', 'utils/index.ts'])
+export default tsupConfig(['cli/index.ts', 'configs/tsup.ts', 'stdlib/index.ts', 'tests/index.ts'])
